9354e72ebfd64e15e5987e599ac14c68123ef15a
alidarovolj <alidarovolzhas@gmail.com>
2024-10-21 11:24:18 +0500

    photos for instruction

--- pages/return/index.vue
+++ pages/return/index.vue
@@ -37,7 +37,7 @@ useHead({
       </p>
       <p class="text-gray-700">{{ $t("warranty_section.description2") }}</p>
     </section>
-    <section>
+    <section class="mb-5">
       <h2 class="text-2xl font-bold text-gray-900 mb-4">
         {{ $t("return.title") }}
       </h2>
@@ -45,6 +45,61 @@ useHead({
         {{ $t("return.description1") }}
       </p>
       <p class="text-gray-700">{{ $t("return.description2") }}</p>
+    </section>
+    <section>
+      <h2 class="text-2xl font-bold text-gray-900 mb-4">
+        Как оформить возврат
+      </h2>
+      <p class="text-gray-700 mb-4">
+        Перед возвратом товара вам необходимо авторизоваться на сайте и иметь
+        оформленные заказы.
+      </p>
+      <div class="flex items-start mb-10 gap-2">
+        <p>1.</p>
+        <div>
+          <p>
+            В верхнем меню вы можете увидеть свое имя, при клике на него, у вас
+            откроется контекстное меню, в котором есть раздел
+            <span class="font-semibold">"Мои заказы"</span>
+          </p>
+          <img
+            class="w-full md:w-2/3"
+            src="@/assets/img/instruction/1.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="flex items-start mb-10 gap-2">
+        <p>2.</p>
+        <div>
+          <p>
+            После того как вы перешли в раздел "Мои заказы", вам необходимо
+            найти заказ, который вы хотите вернуть и нажать на кнопку "Оформить
+            возврат" (он будет доступна в течение 14 дней после покупки), после
+            чего вам откроется форма для заполнения.
+          </p>
+          <img
+            class="w-full md:w-2/3"
+            src="@/assets/img/instruction/2.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="flex items-start gap-2">
+        <p>3.</p>
+        <div>
+          <p>
+            После заполнения формы, вам необходимо отправить товар по адресу,
+            который указан в форме, после чего вам необходимо будет дождаться
+            подтверждения от администратора.
+          </p>
+          <img
+            class="w-full md:w-2/3"
+            src="@/assets/img/instruction/3.png"
+            alt=""
+          />
+        </div>
+      </div>
     </section>
   </div>
 </template>
