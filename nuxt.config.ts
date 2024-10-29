35b0276020dbc320ae838ac5f2e098bbd60ea7f9
alidarovolj <alidarovolzhas@gmail.com>
2024-10-29 12:59:13 +0500

    favicon update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ export default defineNuxtConfig({
         head: {
             link: [
                 { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
                 { rel: 'manifest', href: '/site.webmanifest' }
             ],
             meta: [
